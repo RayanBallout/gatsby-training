6126594a6eb017c023bfceb04bc2d15fd455e937
Rayan Ballout <rayanballout14@gmail.com>
2022-01-17 11:12:53 +0200

    Adding the tags list

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import setupTags from '../utils/setupTags';
+import { Link } from 'gatsby';
 
-const TagsList = () => {
+const TagsList = ({ recipes }) => {
+    const newTags = setupTags(recipes);
     return (
-        <div>
-            Tags List
+        <div className='tag-container'>
+            <h4>recipes</h4>
+            <div className="tags-list">
+                {newTags.map((tag, index) => {
+                    const [text, value] = tag
+                    return <Link to={`/${text}`} key={index}>{text} ({value})</Link>
+                })}
+            </div>
         </div>
     )
 }
